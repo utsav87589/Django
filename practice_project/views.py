d382db9f6c12e34375400e65fb0835e49c399468
utsav87589 <gkundukundu987@gmail.com>
2026-03-10 21:25:25 -0400

    added the about us page and home page, displaying some text

---
+++ practice_project/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+def home(request) : 
+
+    return HttpResponse("This is when I am learning the Django in the middle of ML lol")
+
+def about(response) : 
+
+    return HttpResponse("This is about page, just learning Django lol, like a begineer haha")
