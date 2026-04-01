72c9e5dbb6e73530c091bd7cecadb5bffab30eb6
utsav87589 <gkundukundu987@gmail.com>
2026-03-31 21:55:14 -0400

    user database added

--- Flask/todo_app/app/models.py
+++ Flask/todo_app/app/models.py
@@ -5,4 +5,16 @@ class Task(db.Model) :
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
     status = db.Column(db.String(20), default = 'Pending')
+
+    ### link of the primary key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
     
+
+class User(db.Model) : 
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(25), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+
+    ### checking all the tasks of the user loging in
+    tasks = db.relationship('Task', backref = 'owner', lazy = False)
