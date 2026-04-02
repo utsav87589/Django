4823aba970e5f56d0d1993a94f8bdae54628af1d
utsav87589 <gkundukundu987@gmail.com>
2026-04-01 20:46:12 -0400

    other files updates as well as per the hash passwords and the merge of register route into the auth route

--- Flask/todo_app/app/models.py
+++ Flask/todo_app/app/models.py
@@ -14,7 +14,7 @@ class User(db.Model) :
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(25), nullable = False)
-    password = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(256), nullable = False)
 
     ### checking all the tasks of the user loging in
     tasks = db.relationship('Task', backref = 'owner', lazy = False)
