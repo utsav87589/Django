b20a74058dcc6b9cecb9eec54a6f7d263b8944b1
utsav87589 <gkundukundu987@gmail.com>
2026-04-03 22:30:41 -0400

    added the login and register route and succesfully connected to the database for the register route

--- Flask/note_app/app/routes/auth.py
+++ Flask/note_app/app/routes/auth.py
@@ -11,4 +11,40 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/', methods = ['GET', 'POST'])
 def login() : 
 
+    if request.method == 'POST' : 
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username = username).first()
+
+        if username and password == user.password : 
+            session['user'] = username
+            flash('Login successful', 'sucess')
+            return redirect(url_for('posts.view_posts'))
+        
+        else : 
+            flash('Invalid credentials, try again or register', 'danger')
+
+
     return render_template('auth.html')
+
+
+### register route
+@auth_bp.route('/register', methods = ['GET', 'POST'])
+def register() : 
+
+    if request.method == 'POST' : 
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username and password :
+            new_user = User(username = username, password = password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('Registration successful', 'sucsess')
+
+            return redirect(url_for('auth.login'))
+
+    return render_template('register.html')
