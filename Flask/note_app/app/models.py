44e988ee4e5566d8f821d0c6322e813cd0d74383
utsav87589 <gkundukundu987@gmail.com>
2026-04-05 19:42:35 -0400

    fixed the errors in the database file

--- Flask/note_app/app/models.py
+++ Flask/note_app/app/models.py
@@ -12,9 +12,10 @@ class User(db.Model) :
 
 class Post(db.Model) : 
 
+    id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(200), nullable = False)
     content = db.Column(db.String(2000), nullable = False)
-    status = db.Coumn(db.String(20), deafult = 'active')
+    status = db.Column(db.String(20), default = 'active')
     is_public = db.Column(db.Boolean, default = False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
