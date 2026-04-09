8ccc3da976577ac533be59d84feb1c4bc4cc5232
utsav87589 <gkundukundu987@gmail.com>
2026-04-08 22:20:03 -0400

    data moved to another file, so modified the few imports and paths

--- Flask/note_app/app/routes/home.py
+++ Flask/note_app/app/routes/home.py
@@ -1,25 +1,11 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for, flash
-
-### sample posts
-posts = [
-    {
-    'title' : 'How to learn code',
-    'content' : 'Learning coding is just more than sitting infront of the computer and doing the fancy things shown out there in the movies or somewhere else'
-    },
-    {
-        'title' : 'My 471 day of coding every single day',
-        'content' : 'Somedays are not as easy like walk in the garden, you might been grinding hard for the whole week just to get to a dead end and then have to work all the way back up and forth again!'
-    }
-]
-
-
+from ..data import posts
 
 ### setting up the home app
 home_bp = Blueprint('home', __name__)
 
-
 ### main home route
 @home_bp.route('/')
-def home() : 
+def home() :
 
     return render_template('/tasks/home.html', posts = posts)
--- Flask/note_app/app/routes/posts.py
+++ Flask/note_app/app/routes/posts.py
@@ -9,12 +9,5 @@ posts_bp = Blueprint('posts', __name__)
 ### the basic route to show the posts
 @posts_bp.route('/posts')
 def view_posts() : 
-
-    if 'user' not in session : 
-        return redirect(url_for('auth.login'))
     
-    posts = Post.query.filter_by(user_id = session['user']).all()
-    return render_template('posts.html')
-
-
-### adding a new post
+    return render_template('/tasks/posts.html')
