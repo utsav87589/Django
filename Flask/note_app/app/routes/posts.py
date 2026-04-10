9bd01189b1f97b1a4a774be96256a80838f203ff
utsav87589 <gkundukundu987@gmail.com>
2026-04-09 21:45:13 -0400

    fixed the read more button and the logic to view the full post

--- Flask/note_app/app/routes/posts.py
+++ Flask/note_app/app/routes/posts.py
@@ -1,13 +1,21 @@
 from flask import Blueprint, session, request, render_template, redirect, url_for, flash
 from app import db
 from app.models import User, Post
+from ..data import posts
 
 
 posts_bp = Blueprint('posts', __name__)
 
 
 ### the basic route to show the posts
-@posts_bp.route('/posts')
-def view_posts() : 
-    
-    return render_template('/tasks/posts.html')
+@posts_bp.route('/posts/<int:post_id>')
+def view_posts(post_id) :
+
+    post_to_show = None
+
+    for p in posts: 
+        if p['id'] == post_id:
+            post_to_show = p
+            break
+
+    return render_template('/tasks/posts.html', post = post_to_show)
