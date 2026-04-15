e73193589103bc819debd3e6dd690ada78abbec3
utsav87589 <gkundukundu987@gmail.com>
2026-04-14 21:43:18 -0400

    worked on adding the post, it worked, private vs public still a thing to be fixed

--- Flask/note_app/app/routes/posts.py
+++ Flask/note_app/app/routes/posts.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, session, request, render_template, redirect, url_for, flash
 from app import db
 from app.models import User, Post
+from flask_login import current_user
 
 posts_bp = Blueprint('posts', __name__)
 
@@ -20,7 +21,31 @@ def view_posts(post_id) :
     return render_template('/tasks/posts.html', post = post_to_show)
 
 ### route to edit, add or delete a particular post
-@posts_bp.route('/manage_posts')
+@posts_bp.route('/manage_posts', methods = ['GET', 'POST'])
 def manage_posts() : 
+
+    if current_user.is_authenticated : 
+
+        if request.method == 'POST' : 
+
+            title = request.form.get('post_title')
+            content = request.form.get('post_content')
+            status = request.form.get('status')
+
+            print(f"{title} \n{content} \n{status}")
+
+            is_public = True if status == 'on' else False
+
+            new_post = Post(
+                title = title,
+                content = content,
+                is_public = is_public,
+                user_id = current_user.id
+            )
+
+            db.session.add(new_post)
+            db.session.commit()
+
+            flash('Post created succesfully', redirect(url_for('home.home')))
 
     return render_template('tasks/manage_posts.html')
