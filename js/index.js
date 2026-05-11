abb7dd18975beecf5bac3b05e28a232a6f69c873
utsav87589 <gkundukundu987@gmail.com>
2026-05-11 19:51:36 -0400

    revising the JS

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// console.log('Hello JS! once again');
+
+// window.alert('Revising the JS in the middle of project haha!');
+
+// document.getElementById('myH2').textContent = 'JS Tutorials';
+// document.getElementById('myP').textContent = 'Was making the LSTM based project, but then the need of using a library for each task grew so much that now I am revising the JS to save some time then complete the project and later be in the game with the React as well!';
+
+let name = 'Utsav' ;
+
+console.log(`Your name is ${name}`)
