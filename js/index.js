2ed04eae1563cf3543bad419e36c3bb683e2434a
utsav87589 <gkundukundu987@gmail.com>
2026-05-12 21:11:41 -0400

    revision continues

--- js/index.js
+++ js/index.js
@@ -5,6 +5,19 @@
 // document.getElementById('myH2').textContent = 'JS Tutorials';
 // document.getElementById('myP').textContent = 'Was making the LSTM based project, but then the need of using a library for each task grew so much that now I am revising the JS to save some time then complete the project and later be in the game with the React as well!';
 
-let name = 'Utsav' ;
+// let name = 'Utsav' ;
 
-console.log(`Your name is ${name}`)
+// console.log(`Your name is ${name}`)
+
+// let username;
+
+// document.getElementById('button').onclick = function(){
+//     username = document.getElementById('text').value;
+//     document.getElementById('myH2').textContent = `Hello ${username}`;
+// };
+
+// username = window.prompt('Enter username')
+// document.getElementById('myH2').textContent = `Hello ${username}`;
+
+randomNum = Math.floor(Math.random() * 6) + 1;
+console.log(randomNum)
