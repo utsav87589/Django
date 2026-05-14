75ecdb058f15607df034f3b495c2c46f83271d88
utsav87589 <gkundukundu987@gmail.com>
2026-05-13 20:40:07 -0400

    revision continues with following the tutorial

--- js/index.js
+++ js/index.js
@@ -19,5 +19,30 @@
 // username = window.prompt('Enter username')
 // document.getElementById('myH2').textContent = `Hello ${username}`;
 
-randomNum = Math.floor(Math.random() * 6) + 1;
-console.log(randomNum)
+// randomNum = Math.floor(Math.random() * 6) + 1;
+// console.log(randomNum)
+
+// let age = 68;
+
+// if(age <= 18){
+//     console.log('you cannot drive');
+// }
+// else if(age > 18 && age <= 65){
+//     console.log('you can drive');
+// }
+// else{
+//     console.log('Enjoy your life');
+// }
+
+
+// let age = 22;
+
+// age >= 18 ? console.log('You can drive') : console.log('You cannot driver sorry!');
+
+// let isStudent = true;
+// let isStudent = false;
+// let message = isStudent ? 'Yes' : 'No'
+// console.log(message)
+
+let username = 'Utsav ';
+console.log(username.repeat(3))
