5f69440509be67864a3ab7657d29c8958345bf10
utsav87589 <gkundukundu987@gmail.com>
2026-05-20 20:44:08 -0400

    getters&setters, destructuring, nested objects

--- js/index.js
+++ js/index.js
@@ -469,3 +469,87 @@
 // tahoe.run();
 
 
+//-------------getters and setters
+
+
+// class Rectangle{
+
+//     constructor(width, height){
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     set width(newWidth){
+//         if (newWidth > 0){
+//             this._width = newWidth;
+//         }
+//         else{
+//             console.error('Width must be positive number');
+//         }
+//     }
+//     set height(newHeight){
+//         if (newHeight > 0) {
+//             this._height = newHeight;
+//         }
+//         else{
+//             console.error('Height must be a positive number');
+//         }
+//     }
+
+//     get width(){
+//         return this._width;
+//     }
+
+//     get height(){
+//         return this._height;
+//     }
+
+// }
+
+
+// const rectangle = new Rectangle('pizza', 'burger');
+// const rectangle = new Rectangle(4, 3);
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+
+
+//--------------------Destructuring
+
+// const car = {
+//     'make' : 'Toyota',
+//     'year' : 2026
+// }
+
+// const {make, year, model = 'Camry XSE AWD'} = car;
+
+// console.log(`${make} ${model} ${year}`);
+
+// const numbers = [22, 24, 27, 29];
+
+// const [first, second, third, fourth] = numbers;
+// console.log(`${first} ${second} ${third} ${fourth}`)
+
+
+//------------Nested objects
+
+// const user = {
+//     'name' : 'Tommy',
+//     'age' : 24,
+//     'info' : {
+//         'address' : '231 cedar heights Northbay',
+//         'occupation' : 'Programmer'
+//     }
+// }
+
+// console.log(user.name);
+// console.log(user.info);
+// console.log(user.info.address);
+
+// const {name, info} = user;
+
+// console.log(name);
+// console.log(info);
+
+
+//-------------
