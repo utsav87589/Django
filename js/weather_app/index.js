0c781deafd0c71394fed3646a373339e9006f75a
utsav87589 <gkundukundu987@gmail.com>
2026-06-04 21:33:43 -0400

    work started on js files

--- js/weather_app/index.js
+++ js/weather_app/index.js
@@ -1,61 +1,12 @@
-// loading and checking the api_key
 
 const api_key = CONFIG.OPEN_WEATHER_API_KEY;
-console.log(api_key);
 
+const button = document.querySelector(".submitFormButton");
 
-async function fetch_long_lat(city_name){
+button.addEventListener("click", (event) => {
 
-    try{
+    event.preventDefault();
 
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${api_key}`);
-
-        if(!response.ok){
-            throw new Error('Could not fetch the resources');
-        }
-
-        const geo_data = await response.json();
-
-        return{lat : geo_data[0].lat, 
-            lon : geo_data[0].lon};
-    }
-    catch(error){
-        console.error(error);
-    }
-}
-
-
-async function fetch_weather_by_coord(lat, lon){
-    try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`)
-
-        if(!response.ok){
-            throw new Error('Could not complete the request');
-        }
-
-        const weather_data = await response.json();
-
-        return weather_data;
-    }
-    catch(error){
-        console.error(error);
-    }
-}
-
-async function fetch_weather_by_city(city_name){
-
-    const {lat, lon} = await fetch_long_lat(city_name);
-    const weather = await fetch_weather_by_coord(lat, lon);
-
-    console.log(weather);
-    console.log(weather['main'].temp);
-    console.log(weather['weather'][0].description);
-    console.log(`${city_name} :: ${lat} :: ${lon}`);
-}
-
-
-fetch_weather_by_city('Montreal');
-fetch_weather_by_city('Atlanta');
-fetch_weather_by_city('London');
-fetch_weather_by_city('Miami');
-fetch_weather_by_city('Las Vegas');
+    const cityName = document.querySelector(".cityInput").value;
+    console.log(cityName);
+})
