8a4777494a304c8de4b8aa389f5fd05cac687924
utsav87589 <gkundukundu987@gmail.com>
2026-06-07 12:27:30 -0400

    if no city name entered, wrong city name entered handled, logic for displaying the wether icon as well added, toogling the hidden and display state of the wether card also done

--- js/weather_app/index.js
+++ js/weather_app/index.js
@@ -1,22 +1,47 @@
 const api_key = CONFIG.OPEN_WEATHER_API_KEY;
 
 const button = document.querySelector(".submitFormButton");
+const displayCard = document.querySelector(".displayCard");
 
 button.addEventListener("click", async (event) => {
 
     event.preventDefault();
     const cityName = document.querySelector(".cityInput").value;
 
-    const{temperature, humidity, description, id} = await getCityWeatherByName(cityName);
+    if(!cityName){
+        displayCard.classList.add("hidden");
+        window.alert("Enter a city name please!");
+    }
 
-    document.querySelector('.displayCity').textContent = cityName;
-    document.querySelector('.displayTemp').textContent = `Temp : ${temperature}°C`;
-    document.querySelector('.displayHumidity').textContent = `Humidity : ${humidity}%`;
-    document.querySelector('.displayDesc').textContent = description;
+    try{
+        const{temperature, humidity, description, iconId} = await getCityWeatherByName(cityName);
+        displayCard.classList.remove("hidden");
+
+        const weatherIconUrl = `https://openweathermap.org/payload/api/media/file/${iconId}.png`;
+        const displayIcon = document.querySelector(".displayIcon");
+
+        displayIcon.src = `${weatherIconUrl}`;
+        displayIcon.alt = `${description}`;
+
+        displayIcon.classList.remove("hidden");
+
+        document.querySelector(".displayCity").textContent = cityName;
+        document.querySelector(".displayTemp").textContent = `Temp : ${temperature}°C`;
+        document.querySelector(".displayHumidity").textContent = `Humidity : ${humidity}%`;
+        document.querySelector(".displayDesc").textContent = `${description}`;
+
+    }
+
+    catch(error){
+        console.error(error);
+        displayCard.classList.add("hidden");
+        window.alert("Please check the city name!");
+    }
     
 })
 
 async function fetchCityGeoCoordinates(cityName){
+
     try{
         const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${api_key}`);
 
@@ -49,7 +74,7 @@ async function fetchCityWeatherByCoordinates(latitude, longitude){
                 'temperature' : cityWeather['main'].temp,
                 'humidity' : cityWeather['main'].humidity,
                 'description' : cityWeather['weather'][0].description,
-                'id' : cityWeather['weather'][0].id
+                'iconId' : cityWeather['weather'][0].icon
             }
         }
     }
@@ -62,10 +87,10 @@ async function getCityWeatherByName(cityName){
 
     try{
         const {latitude, longitude} = await fetchCityGeoCoordinates(cityName);
-        const {temperature, humidity, description, id} = await fetchCityWeatherByCoordinates(latitude, longitude);
+        const {temperature, humidity, description, iconId} = await fetchCityWeatherByCoordinates(latitude, longitude);
 
         return{
-            temperature, humidity, description, id
+            temperature, humidity, description, iconId
         }
 
     }
