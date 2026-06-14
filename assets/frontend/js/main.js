52ba42e14d5d3af2932ff11292900888234e2a19
utsav87589 <gkundukundu987@gmail.com>
2026-06-14 00:23:00 -0400

    started working separately on the frontend and backend, later will join directly in the project, edit : the code is from June 13th but committed at 12:22 AM on June 14th

---
+++ assets/frontend/js/main.js
@@ -0,0 +1,8 @@
+loginForm = document.querySelector("#login-form");
+
+if(loginForm){
+    document.addEventListener("submit", (event) => {
+        console.log("button added");
+        window.alert("successfully working the JS and other files");
+    })
+}
