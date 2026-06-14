81f18d53019f6e459c107a14c91254009ab709cd
utsav87589 <gkundukundu987@gmail.com>
2026-06-14 18:08:18 -0400

    js file completed as per the login and register form

--- assets/frontend/js/main.js
+++ assets/frontend/js/main.js
@@ -1,8 +1,39 @@
-loginForm = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
+const registerForm = document.querySelector("#register-form");
+let dashboardContainer = document.querySelector("#dashboard-container");
 
 if(loginForm){
-    document.addEventListener("submit", (event) => {
-        console.log("button added");
-        window.alert("successfully working the JS and other files");
+    loginForm.addEventListener("submit", (event) => {
+
+        event.preventDefault();
+
+        username = document.querySelector("#login-username").value;
+        password = document.querySelector("#login-password").value;
+
+        console.log(`${username} :: ${password}`);
+
+        setTimeout(() => {
+            window.location.href = "../html/dashboard.html";
+        }, 2000);
     })
+}
+
+if(registerForm){
+    registerForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        username = document.querySelector("#register-username").value;
+        password = document.querySelector("#register-password").value;
+
+        console.log(`${username} :: ${password}`);
+        window.alert("Registration succesful!");
+
+        setTimeout(() => {
+            window.location.href = "../html/login.html";
+        }, 3000);
+    })
+}
+
+if(dashboardContainer){
+    
 }
