cee6d7bc2940d63ac76e3d4b4bec904b0e056a72
utsav87589 <gkundukundu987@gmail.com>
2026-06-16 22:25:37 -0400

    logic to add tasks, solid cross out toggle, and remove them added, core frontend is done at this point

--- assets/frontend/js/main.js
+++ assets/frontend/js/main.js
@@ -63,13 +63,67 @@ if(dashboardContainer){
 
             console.log(tasks);
 
+            renderTasks();
             taskInput.value = "";
         }
     })
 
-}
 
+    function renderTasks(){
+        const displayTaskContainer = document.querySelector("#display-tasks");
 
-function renderTasks(){
-    //function that will be responsible to add, remove or cross the tasks
+        displayTaskContainer.innerHTML = "";
+
+        tasks.forEach(task => {
+            const individualTask = document.createElement("div");
+            individualTask.className = "individual-task";
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+
+            const textSpan = document.createElement("span");
+            textSpan.textContent = task.text;
+
+            const removeTask = document.createElement("span");
+            removeTask.innerHTML = "X";
+            removeTask.style.cursor = "pointer";
+            removeTask.style.marginLeft = "15px";
+
+            if (task.isCompleted) {
+                checkbox.checked = true;
+                textSpan.style.textDecoration = "line-through";
+                textSpan.style.opacity = "0.5";
+            }
+
+            checkbox.addEventListener("change", () => {
+                toggleTask(task.id);
+            })
+
+            removeTask.addEventListener("click", () => {
+                deleteTask(task.id);
+            })
+
+            individualTask.appendChild(checkbox);
+            individualTask.appendChild(textSpan);
+            individualTask.appendChild(removeTask);
+
+            displayTaskContainer.appendChild(individualTask);
+        })
+    }
+
+    function toggleTask(id) {
+        tasks = tasks.map(task => {
+            if(task.id === id){
+                return {...task, isCompleted : !task.isCompleted};
+            }
+            return task;
+        })
+        renderTasks();
+    }
+
+    function deleteTask(id){
+        tasks = tasks.filter(task => task.id !== id);
+        renderTasks();
+    }
+
 }
