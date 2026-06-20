3ee691fe9ac1450ef4953ea295e46cbceb5f6d23
utsav87589 <gkundukundu987@gmail.com>
2026-06-19 21:02:05 -0400

    logic for the login and register routes written as per according to the standard apis

--- assets/backend/app/routes/auth.py
+++ assets/backend/app/routes/auth.py
@@ -2,27 +2,77 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from app import db
 from app.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import jsonify
 
 
 auth_bp = Blueprint('auth', __name__)
 
 ### login route
-@auth_bp.route('/')
-@auth_bp.route('/login', methods = ['GET', 'POST'])
+@auth_bp.route('/', methods = ['POST'])
+@auth_bp.route('/login', methods = ['POST'])
 def login() : 
 
-    if request.method == 'POST' : 
-        username = request.form.get('login-username')
-        password = request.form.get('login-password')
+    data = request.get_json()
 
-        print(f"username  : {username} \npassword : {password}")
+    if not data : 
+        return jsonify({"status" : "error", "message" : "missing request data"}), 400
+    
+    username = data.get('username')
+    password = data.get('password')
 
-        return redirect(url_for('dashboard.dashboard'))
+    if not username or not password : 
+        return jsonify({"status" : "error", "message" : "username and password required"}), 400
+    
+    user = User.query.filter_by(username = username).first()
 
-    return render_template('login.html')
+    if user and check_password_hash(user.password, password) : 
+        return jsonify({
+            "status" : "success",
+            "message" : f"login successful, hello {user.username}",
+            "user_id" : user.id
+        }), 200
+    
+    return jsonify({
+        "status" : "error",
+        "message" : "invalid credentials"
+    }), 401
 
-### register route
-@auth_bp.route('/register')
+
+@auth_bp.route("/register", methods = ['POST'])
 def register() : 
 
-    return render_template('register.html')
+    data = request.get_json()
+
+    if not data : 
+        return jsonify({
+            "status" : "error",
+            "message"  : "missing the requested data"
+        }), 400
+    
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password : 
+        return jsonify({
+            "status" : "error",
+            "message" : "missing credentials"
+        }), 400
+    
+    existing_user = User.query.filter_by(username = username).first()
+
+    if existing_user : 
+        return jsonify({
+            "status" : "error",
+            "message" : "username already taken"
+        }), 400
+    
+    password_hashed = generate_password_hash(password)
+
+    new_user = User(username = username, password = password_hashed)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "status" : "success",
+        "message" : f"succesfully created the user : {username}"
+    }), 201
